847e6c55a042e5717536217628f75be13b26d38b
Jure Sunic <juresunic@hotmail.com>
2023-03-12 23:33:57 +0000

    Added blured image and three buttons with search bar

--- src/container/HomePageContainer/Intro/Intro.jsx
+++ src/container/HomePageContainer/Intro/Intro.jsx
@@ -9,7 +9,14 @@ const Intro = () => (
             <div className="app__wrapper_info">
                 <h1 className="app__intro-h1">Mi Znamo Sto Dom<br/>Znaci Za Vas</h1>      
                 <SubHeading title="Pronadji svoj budući dom samo jednim klikom"/>
-                <button type="button" className="custom__button">Explore Menu</button>
+                <div>
+                    <button type="button" className="css-button-neumorphic">Prodaja</button>
+                    <button type="button" className="css-button-neumorphic">Prodaja</button>
+                </div>
+                <div class="ui icon input"><input type="text" placeholder="Search..."/>
+                    <i aria-hidden="true" class="search circular inverted link icon"></i>
+                    <button type="button" className="css-button-neumorphic">Prodaja</button>
+                </div>
             </div>
     </div>
 );
